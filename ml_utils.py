76a1e0837efa848ad09ff14bfca53a5c0fe99767
Nishaa95 <nishaamkce@gmail.com>
2023-01-22 18:12:39 -0700

    update

--- ml_utils.py
+++ ml_utils.py
@@ -3,6 +3,9 @@ import numpy as np
 import math
 import sklearn.datasets
 import ipywidgets as widgets
+from scipy import stats
+
+
 
 ##Seaborn for fancy plots. 
 #%matplotlib inline
@@ -125,20 +128,70 @@ class edaDF:
             figure.show()
         return figure
 
+    def statistics(self):
+        return self.data.describe()
+    
+    def outliers(self):
+        threshold=3
+        columns= self.data.select_dtypes(include=['int','float']).columns
+        for column in columns:
+            z_score= np.abs(stats.zscore(self.data[column]))
+            outlier = np.where(z_score> threshold)
+            print (outlier)
+    def boxplot(self):
+        return self.data.boxplot(grid= False, rot= 45, fontsize =15)
+    
+
+    def valuecounts(self):
+        for column in self.cat:
+            print (self.data[column].value_counts())
+            print('\n')
+
+    def missing(self):
+        return self.data.isnull().sum()
+
+    def pairplot(self):
+        return sns.pairplot(self.data)
+
+    def heatmap(self):
+        corr = self.data.corr()
+        heatmap= sns.heatmap(corr, annot= True)
+        return heatmap
+    
+  
+
+
+
     def fullEDA(self):
         out1 = widgets.Output()
         out2 = widgets.Output()
         out3 = widgets.Output()
         out4 = widgets.Output()
+        out5 = widgets.Output()
+        out6 = widgets.Output()
+        out7 = widgets.Output()
+        out8 = widgets.Output()
+        out9 = widgets.Output()
+        out10 = widgets.Output()
+        
 
-        tab = widgets.Tab(children = [out1, out2, out3])
+        tab = widgets.Tab(children = [out1, out2, out3,out4,out5,out6,out7,out8,out9,out10])
         tab.set_title(0, 'Info')
         tab.set_title(1, 'Categorical')
         tab.set_title(2, 'Numerical')
+        tab.set_title(3, 'Basic stats')
+        tab.set_title(4, 'Outliers')
+        tab.set_title(5, 'Boxplot')
+        tab.set_title(6, 'Value Counts')
+        tab.set_title(7, 'Missing values')
+        tab.set_title(8, 'Pairplot')
+        tab.set_title(9, 'Heatmap')
+
         display(tab)
 
         with out1:
             self.info()
+
 
         with out2:
             fig2 = self.countPlots(splitTarg=True, show=False)
@@ -147,3 +200,29 @@ class edaDF:
         with out3:
             fig3 = self.histPlots(kde=True, show=False)
             plt.show(fig3)
+
+    
+
+        with out4:
+            print(self.statistics())
+        
+        with out5:
+            print(self.outliers())
+
+        with out6:
+            fig7= self.boxplot()
+            plt.show(fig7)
+        
+        with out7:
+            print(self.valuecounts())
+        
+        with out8:
+            print(self.missing())
+        
+        with out9:
+            fig9= self.pairplot()
+            plt.show(fig9)
+
+        with out10:
+            fig10 = self.heatmap()
+            plt.show(fig10)
